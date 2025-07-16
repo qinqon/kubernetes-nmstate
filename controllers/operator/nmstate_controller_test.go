3d38baa23328b595a10b9ecb119747a7b77ea76f
Mat Kowalski <mko@redhat.com>
2025-07-16 12:38:38 +0200

    UPSTREAM: <carry>: netpol: use targeted policy for deny-all
    One of the downstream-only commits removes a line from the
    controllers/operator/nmstate_controller_test.go file.
    
    Here we carry the missing bit from the
    9fcf4ea5132972a3fe2e70b31193c2650df685e9 commit.
    
    (cherry picked from commit a0c58dbb6e244a0f9113023e7710e2cd8f3058fd)
    (cherry picked from commit 8ce85b008962397b4bb96b2b4e023332d5820ee1)
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- controllers/operator/nmstate_controller_test.go
+++ controllers/operator/nmstate_controller_test.go
@@ -211,6 +211,7 @@ var _ = Describe("NMState controller reconcile", func() {
 		reconciler.Scheme = scheme.Scheme
 		reconciler.Log = ctrl.Log.WithName("controllers").WithName("NMState")
 		os.Setenv("HANDLER_NAMESPACE", handlerNamespace)
+		os.Setenv("OPERATOR_NAMESPACE", operatorNamespace)
 		os.Setenv("HANDLER_IMAGE", handlerImage)
 		os.Setenv("HANDLER_IMAGE_PULL_POLICY", imagePullPolicy)
 		os.Setenv("HANDLER_PREFIX", handlerPrefix)
