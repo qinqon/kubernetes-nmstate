8a4acb4bc2e7248ef36282b04daa2721950875f6
Mat Kowalski <mko@redhat.com>
2026-04-07 13:59:27 +0200

    UPSTREAM:<carry>: Revert "fix(e2e): Wait for nodes to reboot (#1419)"
    This reverts commit 138fcfdc26b580485d92e84b9e07a97721d60b4a.
    
    It causes trouble downstream because (we suppose) at restarting the OCP
    node it takes more time for k-nmstate to reach its readiness.
    
    In the future (if upstream test is more robust) we can consider
    reverting this revert.
    
    (cherry picked from commit e0a06fc7d2bd9f13a2fb67e5608bdb5567b94b11)
    (cherry picked from commit 0ee0aba1eca98636ee965c6918ec6cd9359941cf)
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- test/e2e/handler/utils.go
+++ test/e2e/handler/utils.go
@@ -358,12 +358,13 @@ func restartNodeWithoutWaiting(node string) {
 func waitForNodeToStart(node string) {
 	Byf("Waiting till node %s is rebooted", node)
 	// It will wait till uptime -p will return up that means that node was currently rebooted and is 0 min up
-	Eventually(func(g Gomega) string {
+	Eventually(func() string {
 		output, err := runner.RunAtNode(node, "uptime", "-p")
-		// Using g.Expect makes Eventually retry on errors directly
-		g.Expect(err).NotTo(HaveOccurred())
+		if err != nil {
+			return "not yet"
+		}
 		return output
-	}, 300*time.Second, 5*time.Second).Should(ContainSubstring("up"), fmt.Sprintf("Node %s failed to start after reboot", node))
+	}, 300*time.Second, 5*time.Second).ShouldNot(Equal("up"), fmt.Sprintf("Node %s failed to start after reboot", node))
 }
 
 func createDummyConnection(nodesToModify []string, dummyName string) []error {
